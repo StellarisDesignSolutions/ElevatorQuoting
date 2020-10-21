74904bda41cc3e9959ebfaca6f0c67dcdfbbacf0
Greg Young <gregyoung03@outlook.com>
2020-10-20 22:40:46 -0400

    First Push

---
+++ ElevatorQuoting/ElevatorQuoting/MainForm.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace ElevatorQuoting
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelClassofLoading = new System.Windows.Forms.Label();
+            this.labelOutputs = new System.Windows.Forms.Label();
+            this.comboxLoadType = new System.Windows.Forms.ComboBox();
+            this.labelX1 = new System.Windows.Forms.Label();
+            this.labelUnit3 = new System.Windows.Forms.Label();
+            this.labelUnit2 = new System.Windows.Forms.Label();
+            this.labelSpeedUnit = new System.Windows.Forms.Label();
+            this.labelUnit5 = new System.Windows.Forms.Label();
+            this.labelUnit4 = new System.Windows.Forms.Label();
+            this.labelUnit1 = new System.Windows.Forms.Label();
+            this.labelLoadType = new System.Windows.Forms.Label();
+            this.labelPlatformSize = new System.Windows.Forms.Label();
+            this.labelTravelSpeed = new System.Windows.Forms.Label();
+            this.labelOverheadCl = new System.Windows.Forms.Label();
+            this.labelPitDepth = new System.Windows.Forms.Label();
+            this.labelTravelDis = new System.Windows.Forms.Label();
+            this.txtboxPlatformLength = new System.Windows.Forms.TextBox();
+            this.txtboxPlatformWidth = new System.Windows.Forms.TextBox();
+            this.txtboxTravelSpeed = new System.Windows.Forms.TextBox();
+            this.txtboxOverheadCl = new System.Windows.Forms.TextBox();
+            this.txtboxPitDepth = new System.Windows.Forms.TextBox();
+            this.txtboxTravelDis = new System.Windows.Forms.TextBox();
+            this.labelNotes = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.labelClassofLoading);
+            this.panel1.Controls.Add(this.labelOutputs);
+            this.panel1.Location = new System.Drawing.Point(494, 118);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 276);
+            this.panel1.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Capacity ";
+            // 
+            // labelClassofLoading
+            // 
+            this.labelClassofLoading.AutoSize = true;
+            this.labelClassofLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClassofLoading.Location = new System.Drawing.Point(58, 62);
+            this.labelClassofLoading.Name = "labelClassofLoading";
+            this.labelClassofLoading.Size = new System.Drawing.Size(57, 16);
+            this.labelClassofLoading.TabIndex = 3;
+            this.labelClassofLoading.Text = "Class: C";
+            // 
+            // labelOutputs
+            // 
+            this.labelOutputs.AutoSize = true;
+            this.labelOutputs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutputs.Location = new System.Drawing.Point(20, 15);
+            this.labelOutputs.Name = "labelOutputs";
+            this.labelOutputs.Size = new System.Drawing.Size(73, 20);
+            this.labelOutputs.TabIndex = 2;
+            this.labelOutputs.Text = "Outputs";
+            // 
+            // comboxLoadType
+            // 
+            this.comboxLoadType.FormattingEnabled = true;
+            this.comboxLoadType.Items.AddRange(new object[] {
+            "Automobile",
+            "Pallettes"});
+            this.comboxLoadType.Location = new System.Drawing.Point(78, 216);
+            this.comboxLoadType.Name = "comboxLoadType";
+            this.comboxLoadType.Size = new System.Drawing.Size(121, 21);
+            this.comboxLoadType.TabIndex = 25;
+            // 
+            // labelX1
+            // 
+            this.labelX1.AutoSize = true;
+            this.labelX1.Location = new System.Drawing.Point(162, 129);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(12, 13);
+            this.labelX1.TabIndex = 23;
+            this.labelX1.Text = "x";
+            // 
+            // labelUnit3
+            // 
+            this.labelUnit3.AutoSize = true;
+            this.labelUnit3.Location = new System.Drawing.Point(244, 133);
+            this.labelUnit3.Name = "labelUnit3";
+            this.labelUnit3.Size = new System.Drawing.Size(15, 13);
+            this.labelUnit3.TabIndex = 22;
+            this.labelUnit3.Text = "in";
+            // 
+            // labelUnit2
+            // 
+            this.labelUnit2.AutoSize = true;
+            this.labelUnit2.Location = new System.Drawing.Point(131, 133);
+            this.labelUnit2.Name = "labelUnit2";
+            this.labelUnit2.Size = new System.Drawing.Size(15, 13);
+            this.labelUnit2.TabIndex = 21;
+            this.labelUnit2.Text = "in";
+            // 
+            // labelSpeedUnit
+            // 
+            this.labelSpeedUnit.AutoSize = true;
+            this.labelSpeedUnit.Location = new System.Drawing.Point(271, 327);
+            this.labelSpeedUnit.Name = "labelSpeedUnit";
+            this.labelSpeedUnit.Size = new System.Drawing.Size(23, 13);
+            this.labelSpeedUnit.TabIndex = 20;
+            this.labelSpeedUnit.Text = "ft/s";
+            // 
+            // labelUnit5
+            // 
+            this.labelUnit5.AutoSize = true;
+            this.labelUnit5.Location = new System.Drawing.Point(271, 405);
+            this.labelUnit5.Name = "labelUnit5";
+            this.labelUnit5.Size = new System.Drawing.Size(15, 13);
+            this.labelUnit5.TabIndex = 19;
+            this.labelUnit5.Text = "in";
+            // 
+            // labelUnit4
+            // 
+            this.labelUnit4.AutoSize = true;
+            this.labelUnit4.Location = new System.Drawing.Point(149, 405);
+            this.labelUnit4.Name = "labelUnit4";
+            this.labelUnit4.Size = new System.Drawing.Size(15, 13);
+            this.labelUnit4.TabIndex = 18;
+            this.labelUnit4.Text = "in";
+            // 
+            // labelUnit1
+            // 
+            this.labelUnit1.AutoSize = true;
+            this.labelUnit1.Location = new System.Drawing.Point(149, 327);
+            this.labelUnit1.Name = "labelUnit1";
+            this.labelUnit1.Size = new System.Drawing.Size(15, 13);
+            this.labelUnit1.TabIndex = 17;
+            this.labelUnit1.Text = "in";
+            // 
+            // labelLoadType
+            // 
+            this.labelLoadType.AutoSize = true;
+            this.labelLoadType.Location = new System.Drawing.Point(75, 200);
+            this.labelLoadType.Name = "labelLoadType";
+            this.labelLoadType.Size = new System.Drawing.Size(58, 13);
+            this.labelLoadType.TabIndex = 24;
+            this.labelLoadType.Text = "Load Type";
+            // 
+            // labelPlatformSize
+            // 
+            this.labelPlatformSize.AutoSize = true;
+            this.labelPlatformSize.Location = new System.Drawing.Point(75, 107);
+            this.labelPlatformSize.Name = "labelPlatformSize";
+            this.labelPlatformSize.Size = new System.Drawing.Size(68, 13);
+            this.labelPlatformSize.TabIndex = 16;
+            this.labelPlatformSize.Text = "Platform Size";
+            // 
+            // labelTravelSpeed
+            // 
+            this.labelTravelSpeed.AutoSize = true;
+            this.labelTravelSpeed.Location = new System.Drawing.Point(204, 304);
+            this.labelTravelSpeed.Name = "labelTravelSpeed";
+            this.labelTravelSpeed.Size = new System.Drawing.Size(71, 13);
+            this.labelTravelSpeed.TabIndex = 15;
+            this.labelTravelSpeed.Text = "Travel Speed";
+            // 
+            // labelOverheadCl
+            // 
+            this.labelOverheadCl.AutoSize = true;
+            this.labelOverheadCl.Location = new System.Drawing.Point(204, 382);
+            this.labelOverheadCl.Name = "labelOverheadCl";
+            this.labelOverheadCl.Size = new System.Drawing.Size(105, 13);
+            this.labelOverheadCl.TabIndex = 14;
+            this.labelOverheadCl.Text = "Overhead Clearance";
+            // 
+            // labelPitDepth
+            // 
+            this.labelPitDepth.AutoSize = true;
+            this.labelPitDepth.Location = new System.Drawing.Point(82, 382);
+            this.labelPitDepth.Name = "labelPitDepth";
+            this.labelPitDepth.Size = new System.Drawing.Size(51, 13);
+            this.labelPitDepth.TabIndex = 13;
+            this.labelPitDepth.Text = "Pit Depth";
+            // 
+            // labelTravelDis
+            // 
+            this.labelTravelDis.AutoSize = true;
+            this.labelTravelDis.Location = new System.Drawing.Point(82, 304);
+            this.labelTravelDis.Name = "labelTravelDis";
+            this.labelTravelDis.Size = new System.Drawing.Size(82, 13);
+            this.labelTravelDis.TabIndex = 12;
+            this.labelTravelDis.Text = "Travel Distance";
+            // 
+            // txtboxPlatformLength
+            // 
+            this.txtboxPlatformLength.Location = new System.Drawing.Point(191, 126);
+            this.txtboxPlatformLength.Name = "txtboxPlatformLength";
+            this.txtboxPlatformLength.Size = new System.Drawing.Size(47, 20);
+            this.txtboxPlatformLength.TabIndex = 10;
+            // 
+            // txtboxPlatformWidth
+            // 
+            this.txtboxPlatformWidth.Location = new System.Drawing.Point(78, 126);
+            this.txtboxPlatformWidth.Name = "txtboxPlatformWidth";
+            this.txtboxPlatformWidth.Size = new System.Drawing.Size(47, 20);
+            this.txtboxPlatformWidth.TabIndex = 9;
+            // 
+            // txtboxTravelSpeed
+            // 
+            this.txtboxTravelSpeed.Location = new System.Drawing.Point(218, 320);
+            this.txtboxTravelSpeed.Name = "txtboxTravelSpeed";
+            this.txtboxTravelSpeed.Size = new System.Drawing.Size(47, 20);
+            this.txtboxTravelSpeed.TabIndex = 8;
+            // 
+            // txtboxOverheadCl
+            // 
+            this.txtboxOverheadCl.Location = new System.Drawing.Point(218, 398);
+            this.txtboxOverheadCl.Name = "txtboxOverheadCl";
+            this.txtboxOverheadCl.Size = new System.Drawing.Size(47, 20);
+            this.txtboxOverheadCl.TabIndex = 7;
+            // 
+            // txtboxPitDepth
+            // 
+            this.txtboxPitDepth.Location = new System.Drawing.Point(96, 398);
+            this.txtboxPitDepth.Name = "txtboxPitDepth";
+            this.txtboxPitDepth.Size = new System.Drawing.Size(47, 20);
+            this.txtboxPitDepth.TabIndex = 6;
+            // 
+            // txtboxTravelDis
+            // 
+            this.txtboxTravelDis.Location = new System.Drawing.Point(96, 320);
+            this.txtboxTravelDis.Name = "txtboxTravelDis";
+            this.txtboxTravelDis.Size = new System.Drawing.Size(47, 20);
+            this.txtboxTravelDis.TabIndex = 11;
+            // 
+            // labelNotes
+            // 
+            this.labelNotes.AutoSize = true;
+            this.labelNotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNotes.Location = new System.Drawing.Point(256, 32);
+            this.labelNotes.Name = "labelNotes";
+            this.labelNotes.Size = new System.Drawing.Size(239, 29);
+            this.labelNotes.TabIndex = 5;
+            this.labelNotes.Text = "Type B Material Lift";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboxLoadType);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.labelUnit3);
+            this.Controls.Add(this.labelUnit2);
+            this.Controls.Add(this.labelSpeedUnit);
+            this.Controls.Add(this.labelUnit5);
+            this.Controls.Add(this.labelUnit4);
+            this.Controls.Add(this.labelUnit1);
+            this.Controls.Add(this.labelLoadType);
+            this.Controls.Add(this.labelPlatformSize);
+            this.Controls.Add(this.labelTravelSpeed);
+            this.Controls.Add(this.labelOverheadCl);
+            this.Controls.Add(this.labelPitDepth);
+            this.Controls.Add(this.labelTravelDis);
+            this.Controls.Add(this.txtboxPlatformLength);
+            this.Controls.Add(this.txtboxPlatformWidth);
+            this.Controls.Add(this.txtboxTravelSpeed);
+            this.Controls.Add(this.txtboxOverheadCl);
+            this.Controls.Add(this.txtboxPitDepth);
+            this.Controls.Add(this.txtboxTravelDis);
+            this.Controls.Add(this.labelNotes);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelClassofLoading;
+        private System.Windows.Forms.Label labelOutputs;
+        private System.Windows.Forms.ComboBox comboxLoadType;
+        private System.Windows.Forms.Label labelX1;
+        private System.Windows.Forms.Label labelUnit3;
+        private System.Windows.Forms.Label labelUnit2;
+        private System.Windows.Forms.Label labelSpeedUnit;
+        private System.Windows.Forms.Label labelUnit5;
+        private System.Windows.Forms.Label labelUnit4;
+        private System.Windows.Forms.Label labelUnit1;
+        private System.Windows.Forms.Label labelLoadType;
+        private System.Windows.Forms.Label labelPlatformSize;
+        private System.Windows.Forms.Label labelTravelSpeed;
+        private System.Windows.Forms.Label labelOverheadCl;
+        private System.Windows.Forms.Label labelPitDepth;
+        private System.Windows.Forms.Label labelTravelDis;
+        private System.Windows.Forms.TextBox txtboxPlatformLength;
+        private System.Windows.Forms.TextBox txtboxPlatformWidth;
+        private System.Windows.Forms.TextBox txtboxTravelSpeed;
+        private System.Windows.Forms.TextBox txtboxOverheadCl;
+        private System.Windows.Forms.TextBox txtboxPitDepth;
+        private System.Windows.Forms.TextBox txtboxTravelDis;
+        private System.Windows.Forms.Label labelNotes;
+    }
+}
+
